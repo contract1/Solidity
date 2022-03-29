5738b4ae72f64726e5db5aa183e3e2329e637f3e
Marcelo Becker <69699575+contract1@users.noreply.github.com>
2022-03-29 10:37:43 -0300

    Update ContractInterface.h

--- test/contracts/ContractInterface.h
+++ test/contracts/ContractInterface.h
@@ -1,5 +1,5 @@
-/*
-	This file is part of solidity.
+
+/*	This file is part of solidity.
 
 	solidity is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
